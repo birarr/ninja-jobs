51cf0d78f8e10df4c0fe6d3e7b747729d4dc2597
Ubiratan da Rosa Rodrigues <ubiratan@192.168.1.5>
2022-02-09 20:25:43 -0300

    Initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi suscipit
+      aliquid sit inventore aspernatur? Eaque accusantium culpa sint corporis
+      voluptates! Enim animi id fugit quae officia dolorem harum iure, tempora
+      assumenda quam, modi nesciunt dolores commodi sit pariatur consectetur
+      debitis fugiat adipisci nobis dolore? Dolore obcaecati ad harum
+      repudiandae aperiam explicabo magnam omnis voluptas expedita. Expedita
+      obcaecati autem commodi laudantium vero, cum, sint quos odit nemo
+      praesentium esse nostrum ullam molestias quasi, ratione reiciendis at?
+      Aliquid porro nesciunt minima neque, reprehenderit earum veniam blanditiis
+      doloribus aut quia corporis, tempora id est soluta placeat voluptas
+      adipisci illo alias, esse nemo fugit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi suscipit
+      aliquid sit inventore aspernatur? Eaque accusantium culpa sint corporis
+      voluptates! Enim animi id fugit quae officia dolorem harum iure, tempora
+      assumenda quam, modi nesciunt dolores commodi sit pariatur consectetur
+      debitis fugiat adipisci nobis dolore? Dolore obcaecati ad harum
+      repudiandae aperiam explicabo magnam omnis voluptas expedita. Expedita
+      obcaecati autem commodi laudantium vero, cum, sint quos odit nemo
+      praesentium esse nostrum ullam molestias quasi, ratione reiciendis at?
+      Aliquid porro nesciunt minima neque, reprehenderit earum veniam blanditiis
+      doloribus aut quia corporis, tempora id est soluta placeat voluptas
+      adipisci illo alias, esse nemo fugit.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste omnis
+      voluptates illo voluptas esse possimus minima impedit atque natus aperiam
+      fuga perspiciatis, veritatis quas ducimus voluptate ratione est, nostrum,
+      maiores voluptatum eius mollitia consequatur praesentium amet. Esse eum
+      officiis, quos eos temporibus repellendus praesentium obcaecati odio
+      dolorem doloremque eaque ipsum, dolores aliquid optio ea cum perspiciatis
+      explicabo id expedita voluptates cupiditate repellat ut mollitia fugiat.
+      Magnam doloribus iure assumenda nulla illum quo numquam adipisci eum,
+      maxime placeat voluptatibus ipsum sint eaque impedit quam illo nobis, eos
+      ducimus expedita reprehenderit quisquam inventore temporibus obcaecati?
+      Molestiae dignissimos, qui tenetur recusandae blanditiis fuga.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste omnis
+      voluptates illo voluptas esse possimus minima impedit atque natus aperiam
+      fuga perspiciatis, veritatis quas ducimus voluptate ratione est, nostrum,
+      maiores voluptatum eius mollitia consequatur praesentium amet. Esse eum
+      officiis, quos eos temporibus repellendus praesentium obcaecati odio
+      dolorem doloremque eaque ipsum, dolores aliquid optio ea cum perspiciatis
+      explicabo id expedita voluptates cupiditate repellat ut mollitia fugiat.
+      Magnam doloribus iure assumenda nulla illum quo numquam adipisci eum,
+      maxime placeat voluptatibus ipsum sint eaque impedit quam illo nobis, eos
+      ducimus expedita reprehenderit quisquam inventore temporibus obcaecati?
+      Molestiae dignissimos, qui tenetur recusandae blanditiis fuga.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
